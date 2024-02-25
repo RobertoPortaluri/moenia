cd2b24dc7ec69e82231ecb62492ac8688b130699
Roberto Portaluri <roberto.portaluri.dev@gmail.com>
2024-02-25 16:20:01 +0100

    test hoempage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+const nextConfig = {
+    // Sovrascrivi le opzioni di generazione HTML predefinite
+    async headers() {
+      return [
+        // Definisci un'intestazione personalizzata per indicare che il file HTML deve essere servito come homepage
+        {
+            source: '/',
+            headers: [
+              {
+                key: "Location",
+                value: "/Home",
+              },
+            ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
